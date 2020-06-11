2545e86e78cc42da378fef9cf5c99a8faf5a07a7
AJ <42003122+AJ-7885@users.noreply.github.com>
2020-06-11 20:55:43 +0200

    gh-pages demo

--- config.rb
+++
@@ -1,7 +0,0 @@
-require "susy"
-css_dir = 'public_html/_/css'
-sass_dir = 'public_html/_/components/sass'
-javascripts_dir = 'public_html/_/js'
-output_style = :compressed
-relative_assets = true;
-sourcemap = true;
